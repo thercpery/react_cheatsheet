d0f205db3169d68fd856d973be5d7ad6152c46a0
Roy Christian Pery <thercpery@gmail.com>
2022-01-02 18:26:02 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
+import About from "./components/About"
 
-function App() {
+
+const App = () => {
+  // const name = "RC";
+  // const x = false;
+  const [showAddTask, setShowAddTask] = useState(true)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // PUT YOUR EVENTS HERE
+
+  // Fetch tasks 
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks")
+    const data = await res.json()
+    // console.log(data)
+    return data
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    // console.log(data)
+    return data
+  }
+
+
+  // Add task
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    // console.log(task)
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([ ...tasks, newTask ])
+    
+  }
+  // Delete task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    })
+    // console.log("delete", id)
+    setTasks(
+      tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    // console.log(id)
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+    setTasks(
+      tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header 
+          onAdd={() => setShowAddTask(!showAddTask)} 
+          showAdd={showAddTask}/>
+        <Routes>
+            <Route path="/" exact element={
+              <>
+              {showAddTask && <AddTask onAdd ={addTask}/>}
+              <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}></Tasks>
+              </>
+            }/>
+            <Route path="/about" element={<About/>}/>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
+
+// Class-based
+// class App extends React.Component{
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
